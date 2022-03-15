fd4419e9e81b83ac70560bfd38a1d28843332f39
Diego Quintana <diequint99@gmail.com>
2022-03-15 13:09:48 -0600

    Langing page and routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import {Home} from "./pages/Home";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			Fantasi
-		</div>
+		<Routes>
+            <Route exact path="/" element={<Home/>}/>
+        </Routes>
 	);
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import styles from "./Home.module.css";
+
+export function Home() {
+	return(<><h2>Bienvenido a Fantasi</h2>
+	<main className={styles.main}>
+		<p>Fantasi es una herramienta ideal para la rápida creación de sitios web personales, así como empresariales, tiendas online, sitios de noticias, paginas gubernamentales, etc.</p>
+		<img className={styles.image} src="./resources/create-pages.png" alt="Ups, aquí iba ina imagen :c"/>
+		<br/>
+		<div className={styles.divisionH}>
+			<div>
+				<img className={styles.halfimage} src="./resources/programing.jpg" alt="Ups, aquí iba ina imagen :c"/>
+			</div>
+			<div>
+				<p>Con nosotros no tienes que ser un diseñador, mucho menos un programador para obtener resultados profesionales. Aunque si lo eres, te va a encantar lo que podrás lograr aquí.<br/><br/>Contamos con decenas de plantillas distintas que se acoplarán perfecto a tus necesidades.<br/><br/>Además, cuando terminas tu sitio, te damos todo el código fuente para que hagas con el lo que quieras.</p>
+			</div>
+		</div>
+		<div className={styles.divisionH}>
+			<div>
+				<p><br/>Por si esto fuera poco, nosotros solo generamos ingresos por publicidad y donativos por lo que NO necesitas pagar absolutamente nada, así, sin letras chiquitas podrás obtener tu sitio ideal</p>
+			</div>
+			<div>
+				<img className={styles.littleimage} src="./resources/no-card.jpg" alt="Ups, aquí iba ina imagen :c"/>
+			</div>
+		</div>
+	</main>
+	</>);
+}
