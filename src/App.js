e2ddd8187ecac141edf72bb379b1b950f82c40b7
Diego Quintana <diequint99@gmail.com>
2022-03-16 21:07:55 -0600

    Designs grid with data taken from json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {Home} from "./pages/Home";
+import {Grid} from "./modules/Grid";
 import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
 		<Routes>
-            <Route exact path="/" element={<Home/>}/>
+            <Route exact path="/" element={<Grid/>}/>
         </Routes>
 	);
 }
---
+++ src/modules/Grid.jsx
@@ -0,0 +1,23 @@
+import designs from "./designs.json";
+import styles from "./Grid.module.css";
+import {Link} from "react-router-dom";
+
+function Card({design}) {
+    //TODO: Replace AI's cat images with previews of our templates
+    return(<li className={styles.card}>
+        <Link to={"/designs/"+design.id}>
+            <img className={styles.image} src="https://thiscatdoesnotexist.com/" alt="No hay gatitos :c"/>
+        </Link>
+        <p className={styles.text}>{design.name}</p>
+    </li>);
+}
+
+export function Grid() {
+    return(<main>
+        <ul className={styles.designGgrid}>
+            {designs.map((design)=>
+                <Card key={design.id} design={design}/>
+            )}
+        </ul>
+    </main>);
+}
